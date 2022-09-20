b1a27b492b6f979c7583a9a149def6b015446599
Aiman Anizan <anizanaimann1@gmail.com>
2022-09-20 17:34:44 +0800

    Add follow us page

---
+++ pages/FollowUs.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const FollowUs = () => {
+  return (
+    <View>
+      <Text>Follow Us Page</Text>
+    </View>
+  );
+};
