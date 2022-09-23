4f8258fc91d690438f12a6baec21e4b2eee790cb
NazihahAH <106499476+NazihahAH@users.noreply.github.com>
2022-09-23 08:45:37 +0800

    Add files via upload

---
+++ sections/FirstNombor.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+import Navigation from '../components/Navigation';
+import FirstNomborDetails from '../components/FirstNomborDetails';
+
+
+const RateUs = () => {
+  return (
+    <View>
+      <Navigation />
+      <FirstNomborDetails />
+    </View>
+  );
+};
+export default RateUs
